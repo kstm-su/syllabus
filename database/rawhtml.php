b640d8147cea0d86e5e23b323ff85dfe4dfb3fe4
bgpat <bgpat@bgpat.net>
2015-09-20 23:12:37 +0900

    余分なエスケープを削除

--- database/rawhtml.php
+++ database/rawhtml.php
@@ -7,8 +7,8 @@ $db = new DBAdmin();
 $q = $db->selectAll('list');
 while ($row = $q->fetch_assoc()) {
 	$raw = file_get_contents(HTML_URL . "?NENDO={$row['year']}&BUKYOKU={$row['department']}&CODE={$row['code']}");
-	$text = $db->escape(mb_convert_encoding($raw, 'utf8', 'cp932'));
-	$db->replace('rawhtml', array($row['id'], $text));
+	$html = mb_convert_encoding($raw, 'utf8', 'cp932');
+	$db->replace('rawhtml', array($row['id'], $html));
 	echo "\033[9D\033[2K{$row['id']}";
 }
 
--- database/rawtext.php
+++ database/rawtext.php
@@ -7,7 +7,7 @@ $db = new DBAdmin();
 $q = $db->selectAll('list');
 while ($row = $q->fetch_assoc()) {
 	$raw = file_get_contents(TEXT_URL . "?NENDO={$row['year']}&BUKYOKU={$row['department']}&CODE={$row['code']}");
-	$html = $db->escape(mb_convert_encoding($raw, 'utf8', 'cp932'));
+	$html = mb_convert_encoding($raw, 'utf8', 'cp932');
 	$db->replace('rawtext', array('id' => $row['id'], 'raw' => $html));
 	echo "\033[9D\033[2K{$row['id']}";
 }
