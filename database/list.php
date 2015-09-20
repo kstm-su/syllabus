fd4fe72d0f0b92bd855f0181acc2f3376a101e67
bgpat <bgpat@bgpat.net>
2015-09-20 18:14:29 +0900

    list.phpの全体的な見直し

--- database/list.php
+++ database/list.php
@@ -1,68 +1,62 @@
 <?php
 
+include_once('./util.php');
 include_once('./db.php');
-
-$data = array(
-	'MODE' => '0',
-	'STARTNO' => '0',
-	'NENDO' => '2015',
-	'CODE_JYOUKEN' => '0',
-	'BtKENSAKU' => '1'
-);
-
-$curl_option = array(
-	CURLOPT_URL => 'https://campus-2.shinshu-u.ac.jp/syllabus/syllabus.dll/Search',
-	CURLOPT_SSL_VERIFYPEER => FALSE,
-	CURLOPT_SSL_VERIFYHOST => FALSE,
-	CURLOPT_RETURNTRANSFER => TRUE
-);
 
 $db = new DBAdmin();
 
-$i = 0;
-$array = array();
-$lecture_array = array();
-
+/* 年度の取得と部局リストの更新 */
+$src = file_get_contents(SEARCH_URL);
+$html = htmlobject($src);
+$year = (int)$html->body->form->table->tbody->tr->td->table->tbody->tr[1]->td[1]->input['value'];
+$departments = $html->body->form->table->tbody->tr->td->table->tbody->tr[0]->td[1]->select->option;
 $db->begin();
-do {
-	$curl = curl_init();
-	$curl_option[CURLOPT_POSTFIELDS] = http_build_query($data);
-	curl_setopt_array($curl, $curl_option);
-	$original_output = curl_exec($curl);
-	$utf_encoded = mb_convert_encoding($original_output, 'utf8', 'cp932');
-	$utf_output = $utf_encoded;
-	//print $utf_output;
-	if (!$i) {
-		array_pop($data);
-		$data += array(
-			'BtNEXT' => '1',
-			'BtKENSAKU' => '1'
-		);
+foreach ($departments as $department) {
+	$code = (string)$department['value'];
+	if ($code) {
+		$desc = (string)$department;
+		$db->insert('department', array(NULL, $code, $desc));
 	}
-	$data['STARTNO'] = $i * 100;
-	$i++;
+}
+$db->commit();
 
-	$search_word = 'Text?';
-	while ($utf_output = strstr($utf_output, $search_word)) {
-		$memo_output = $utf_output;
-		$memo_output = substr($memo_output, 0, strpos($memo_output,'">'));
-		$memo_output = substr($memo_output, strlen($search_word));
-		//echo $memo_output.PHP_EOL;
-		{
-			$sql_txt = explode('&', $memo_output);
-			$sql_array = array();
-			$sql_query = 'INSERT INTO list VALUES (NULL,';
-			foreach($sql_txt as $sql_each_txt){
-				$sql_array = array_merge($sql_array, explode('=', $sql_each_txt));
-			}
-			$db->insert('list', array(NULL, $sql_array[1], $sql_array[3], $sql_array[5]));
-		}
-		$utf_output = substr($utf_output, 1);
-	}
-	echo $i . PHP_EOL;
-	if (strpos($utf_encoded, "<!--\r\n<INPUT type=\"submit\" name=\"BtNEXT\" value=\"次へ >\" >\r\n-->") !== FALSE) {
+/* 講義データのリストを取得 */
+$postdata = array(
+	'MODE' => 0,
+	'STARTNO' => 0,
+	'NENDO' => $year,
+	'CODE_JYOUKEN' => 0,
+	'BtKENSAKU' => 0
+);
+for ($i = 0, $j = -1;; $j = -1) {
+	$src = post_request(SEARCH_URL, $postdata);
+	$html = htmlobject($src);
+	$tr = $html->body->form->table[1]->tbody->tr;
+	if (count($tr) <= 1) {
+		echo "\033[9D\033[2K$i\n`list` is updated.\n";
 		break;
 	}
-} while ($i < 1000);
-$db->commit();
+	$db->begin();
+	foreach ($tr as $row) {
+		$td = $row->td;
+		if (++$j === 0) {
+			continue;
+		}
+		$q = preg_split('/&|=/', $td[3]->a['href']);
+		$db->replace('list', array(
+			'year' => $q[1],
+			'department' => $q[3],
+			'code' => $q[5],
+			'place' => $td[7]));
+	}
+	$db->commit();
+	$postdata['BtKENSAKU'] = 1;
+	if (isset($postdata['BtNEXT'])) {
+		$postdata['STARTNO'] += $j;
+	}
+	$postdata['BtNEXT'] = 1;
+	$i += $j;
+	echo "\033[9D\033[2K$i";
+}
+
 $db->close();
