2922894290cb2838ce324981edec59a94fbc24e5
bgpat <bgpat@bgpat.net>
2015-09-20 18:12:11 +0900

    データベース名, URLを定数化

--- database/config.sample.php
+++ database/config.sample.php
@@ -1,5 +1,10 @@
 <?php
+/* MySQL */
 const GUEST_USER = 'guest_username';
 const GUEST_PASSWD = 'guest_password';
 const ADMIN_USER = 'admin_username';
 const ADMIN_PASSWD = 'admin_password';
+const DB_NAME = 'syllabus_db';
+
+/* URL */
+const SEARCH_URL = 'http://campus-2.shinshu-u.ac.jp/syllabus/syllabus.dll/Search';
