60e9a0d6761b816b5720ff9dcee6c7a5114da394
bgpat <bgpat@bgpat.net>
2015-09-21 07:38:22 +0900

    idとkeyの重複を回避

--- database/textdata.php
+++ database/textdata.php
@@ -15,6 +15,7 @@ while ($row = $table->fetch_assoc()) {
 	$paragraphs = preg_split('/\-{50}\r?\n(?=【.*?】\r?\n\-{50}\r?\n)/', $text);
 	array_shift($paragraphs);
 
+	$data = array();
 	foreach ($paragraphs as $i => $paragraph) {
 		/* keyとvalueを取り出す */
 		$pattern = '/^【(.*?)】\n(?:\-{50}\n\s*([\s\S]*?))?\s*$/';
@@ -26,16 +27,30 @@ while ($row = $table->fetch_assoc()) {
 			$matches[2] = NULL;
 		}
 		list(, $key, $value) = $matches;
+		if (!$key && !$value) {
+			continue;
+		}
 
 		/* valueの英数字を半角に、カタカナを全角に変換 */
 		if (!is_null($value)) {
 			$value = kana($value);
 		}
 
-		/* DBに追加 */
+		if (isset($data[$key])) {
+			if (!is_null($value)) {
+				$data[$key] .= "\n$value";
+			}
+		} else {
+			$data[$key] = $value;
+		}
+	}
+
+	foreach ($data as $key => $value) {
+	/* DBに追加 */
 		$db->replace('textdata',
 			array('id' => $row['id'], 'key' => $key, 'value' => $value));
 	}
+	echo "\033[9D\033[2K{$row['id']}";
 }
 $db->commit();
 $db->close();
