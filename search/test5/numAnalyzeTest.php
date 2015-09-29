277664774eab02242cf5c2ee74326774d171d9a4
Goryudyuma <umaretekyoumade@gmail.com>
2015-09-29 22:54:30 +0900

    numAnalyzeTestの充実。

--- search/test5/numAnalyzeTest.php
+++ search/test5/numAnalyzeTest.php
@@ -1,12 +1,19 @@
 <?php
 require_once(__dir__."/../searchpre5.php");
 
-if (numAnalyze('abc','5..6')==='(`abc` BETWEEN 5 AND 6)') {
-	echo "numAnalyze('abc','5..6') -> ".numAnalyze('abc','5..6')."  (`abc` BETWEEN 5 AND 6)".PHP_EOL;
+if (numAnalyze('abc','5..6')!=='(`abc` BETWEEN 5 AND 6) ') {
+	echo "numAnalyze('abc','5..6') -> ".numAnalyze('abc','5..6')."  (`abc` BETWEEN 5 AND 6) ".PHP_EOL;
 	exit;
 }
-
-while(1){
-	$x=explode(" ",trim(fgets(STDIN)));
-	echo numAnalyze($x[0],$x[1]).PHP_EOL;
+if (numAnalyze('abc','5..')!=='(`abc` >= 5) ') {
+	echo "numAnalyze('abc','5..') -> ".numAnalyze('abc','5..')."  (`abc` >= 5) ".PHP_EOL;
+	exit;
+}
+if (numAnalyze('abc','..5')!=='(`abc` <= 5) ') {
+	echo "numAnalyze('abc','5..') -> ".numAnalyze('abc','..5')."  (`abc` <= 5) ".PHP_EOL;
+	exit;
+}
+if (numAnalyze('abc','5')!=='(`abc` = 5) ') {
+	echo "numAnalyze('abc','5') -> ".numAnalyze('abc','5')."  (`abc` = 5) ".PHP_EOL;
+	exit;
 }
