2d32476421be728f1116049d2674036928d9d473
bgpat <bgpat@bgpat.net>
2015-10-21 19:10:30 +0900

    テーブルの生成を追加

--- database/init.sh
+++ database/init.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 cd `dirname "$0"`
 
-if test $1 = '' ; then
-	echo "useage: `basename $0` 'db_user' 'db_password'"
-	exit
-fi
-
-read -sp 'Enter MySQL password: ' PASSWORD
+read -sp 'Enter MySQL root password: ' PASSWORD
 echo "\n"
 ADMIN_USER=`php -r "require_once('../lib/config.php');echo ADMIN_USER;"`
 ADMIN_PASSWD=`php -r "require_once('../lib/config.php');echo ADMIN_PASSWD;"`
@@ -14,9 +9,11 @@ GUEST_USER=`php -r "require_once('../lib/config.php');echo GUEST_USER;"`
 GUEST_PASSWD=`php -r "require_once('../lib/config.php');echo GUEST_PASSWD;"`
 DB_NAME=`php -r "require_once('../lib/config.php');echo DB_NAME;"`
 
+mysql -uroot -p$PASSWORD -e"CREATE DATABASE \`$DB_NAME\`" > /dev/null 2>&1
+mysql -uroot -p$PASSWORD $DB_NAME -e"CREATE USER \`$ADMIN_USER\` IDENTIFIED BY '$ADMIN_PASSWD'" > /dev/null 2>&1
+mysql -uroot -p$PASSWORD $DB_NAME -e"CREATE USER \`$GUEST_USER\` IDENTIFIED BY '$GUEST_PASSWD'" > /dev/null 2>&1
+mysql -uroot -p$PASSWORD $DB_NAME -e"GRANT ALL ON $DB_NAME.* FOR \`$ADMIN_USER\`" > /dev/null 2>&1
+mysql -uroot -p$PASSWORD $DB_NAME -e"GRANT SELECT ON $DB_NAME.* FOR \`$GUEST_USER\`" > /dev/null 2>&1
+mysql -uroot -p$PASSWORD $DB_NAME < tables.sql > /dev/null 2>%1
 
-mysql -u$1 -p$PASSWORD -e"CREATE DATABASE \`$DB_NAME\`" > /dev/null 2>&1
-mysql -u$1 -p$PASSWORD $DB_NAME -e"CREATE USER \`$ADMIN_USER\` IDENTIFIED BY PASSWORD '$ADMIN_PASSWORD'" > /dev/null 2>&1
-mysql -u$1 -p$PASSWORD $DB_NAME -e"CREATE USER \`$GUEST_USER\` IDENTIFIED BY PASSWORD '$GUEST_PASSWORD'" > /dev/null 2>&1
-mysql -u$1 -p$PASSWORD $DB_NAME -e"GRANT ALL ON $DB_NAME.* FOR \`$ADMIN_USER\`" > /dev/null 2>&1
-mysql -u$1 -p$PASSWORD $DB_NAME -e"GRANT SELECT ON $DB_NAME.* FOR \`$GUEST_USER\`" > /dev/null 2>&1
+echo 'Next step: update.sh'
