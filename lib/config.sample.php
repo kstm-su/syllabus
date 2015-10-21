7b9a23fb86fd8c72196f0d529f3d8339a26a7429
bgpat <bgpat@bgpat.net>
2015-10-21 16:15:09 +0900

    初期設定の変更

--- lib/config.sample.php
+++ lib/config.sample.php
@@ -1,10 +1,10 @@
 <?php
 /* MySQL */
-const GUEST_USER = 'guest_username';
+const GUEST_USER = 'syllabus_readonly';
 const GUEST_PASSWD = 'guest_password';
-const ADMIN_USER = 'admin_username';
+const ADMIN_USER = 'syllabus_admin';
 const ADMIN_PASSWD = 'admin_password';
-const DB_NAME = 'syllabus_db';
+const DB_NAME = 'syllabus';
 
 /* URL */
 const SEARCH_URL = 'http://campus-2.shinshu-u.ac.jp/syllabus/syllabus.dll/Search';
